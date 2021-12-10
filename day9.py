be4b6e1750ae2028ac473fb086270ce969586ec9
Eris Brabham <mattbrabham@hotmail.com>
2021-12-10 06:53:25 +0000

    day 9 merge

--- day9.py
+++ day9.py
@@ -1,10 +1,15 @@
+import math
+
+grid = []
+
 with open("input.txt") as f:
     grid = [i.strip() for i in f.readlines()]
     grid = [[int(grid[i][j]) for j in range(len(grid[i]))] for i in range(len(grid))]
 
 suml = 0
+
+basin_count = 0
 basins = {}
-totalBasins = 0
 
 for y in range(len(grid)):
     for x in range(len(grid[y])):
@@ -13,16 +18,39 @@ for y in range(len(grid)):
             (x == len(grid[y]) - 1 or grid[y][x+1] > height) and
             (y == 0 or grid[y-1][x] > height) and
             (y == len(grid) - 1 or grid[y+1][x] > height)):
-            #suml += height + 1
+            suml += height + 1
 
-            basins[(y, x)] = totalBasins
-            totalBasins += 1
-
-#print(suml)
-
-print(basins)
+            basins[(x, y)] = basin_count
+            basin_count = basin_count + 1
 
 finished = False
 
 while not finished:
-    pass
+    finished = True
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            height = grid[y][x]
+            
+            if (x, y) in basins:
+                continue
+
+            if (x-1, y) in basins and height != 9:
+                basins[(x, y)] = basins[(x-1, y)]
+                finished = False
+            if (x+1, y) in basins and height != 9:
+                basins[(x, y)] = basins[(x+1, y)]
+                finished = False
+            if (x, y-1) in basins and height != 9:
+                basins[(x, y)] = basins[(x, y-1)]
+                finished = False
+            if (x, y+1) in basins and height != 9:
+                basins[(x, y)] = basins[(x, y+1)]
+                finished = False
+
+basin_size = [0] * basin_count
+
+for basin in basins.values():
+    basin_size[basin] = basin_size[basin] + 1
+
+print(suml)
+print(math.prod(sorted(basin_size)[-3:]))
